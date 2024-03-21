f33d914bdfe72ae7d4eac974bbfd9e6deaf7ef98
João Pedro da Rocha Ramalho <116653976+Jaum97531@users.noreply.github.com>
2024-03-21 16:31:17 -0400

    removed api-key

--- gpt.py
+++ gpt.py
@@ -2,5 +2,5 @@
 class GPT :
     link = "https://api.openai.com/v1/chat/completions"
     id_modelo = "gpt-3.5-turbo"
-    API_KEY = "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"
+    API_KEY = "sk-rMTK9JQCXaFs1SZfga38T3BlbkFJUuWwZu***********"
     headers = {"Authorization": f"Bearer {API_KEY}", "Content-Type": "application/json"}
